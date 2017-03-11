49d168db82561ea5e80de46e08af9358bafe7d7f
Dmitriy Shved <dshved@hotmail.com>
2017-03-11 18:19:04 +0300

    Added vue tabs component

--- app.js
+++ app.js
@@ -27,38 +27,38 @@ app.use(router);
 app.use('/api', api);
 
 if (isDev) {
-    // local variables for all views
-    app.locals.env = NODE_ENV;
-    app.locals.reload = true;
-    // static assets served by webpack-dev-middleware & webpack-hot-middleware for development
-    const webpack = require('webpack');
-    const webpackDevMiddleware = require('webpack-dev-middleware');
-    const webpackHotMiddleware = require('webpack-hot-middleware');
-    const webpackDevConfig = require('./build/webpack.config.js');
+  // local variables for all views
+  app.locals.env = NODE_ENV;
+  app.locals.reload = true;
+  // static assets served by webpack-dev-middleware & webpack-hot-middleware for development
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackDevConfig = require('./build/webpack.config.js');
 
-    const compiler = webpack(webpackDevConfig);
+  const compiler = webpack(webpackDevConfig);
 
-    app.use(webpackDevMiddleware(compiler, {
-        publicPath: webpackDevConfig.output.publicPath,
-        noInfo: true,
-        stats: {
-            colors: true,
-        },
-    }));
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackDevConfig.output.publicPath,
+    noInfo: true,
+    stats: {
+      colors: true,
+    },
+  }));
 
-    app.use(webpackHotMiddleware(compiler));
+  app.use(webpackHotMiddleware(compiler));
 
-    const server = http.createServer(app);
+  const server = http.createServer(app);
 
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-    server.listen(PORT, function(){
-        console.log('App (dev) is now running on PORT '+ PORT +'!');
-    });
+  server.listen(PORT, function(){
+    console.log('App (dev) is now running on PORT '+ PORT +'!');
+  });
 } else {
-    // static assets served by express.static() for production
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.listen(PORT, function () {
-        console.log('App (production) is now running on PORT '+ PORT +'!');
-    });
+  // static assets served by express.static() for production
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.listen(PORT, function () {
+    console.log('App (production) is now running on PORT '+ PORT +'!');
+  });
 }
--- client/login/client-entry.js
+++ client/login/client-entry.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// import VueResource from 'vue-resource';
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+});
