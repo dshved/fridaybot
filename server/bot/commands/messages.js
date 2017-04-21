8956bdc19d142948de3e34d833c342687381f4c0
Dmitry Shved <dshved@hotmail.com>
2017-04-21 10:04:46 +0300

    Added police responsed

--- server/bot/commands/messages.js
+++ server/bot/commands/messages.js
@@ -4,51 +4,61 @@ const messages = [
   {
     messages: ['СКАЖИ'],
     startFrom: true,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./say').inRow,
   }, {
     messages: ['ГОВОРИ'],
     startFrom: true,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./say').inColumn,
   }, {
     messages: ['ТЕКСТ '],
     startFrom: true,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./say').emojiText,
   }, {
     messages: ['РАМКА '],
     startFrom: true,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./say').borderText,
   }, {
     messages: ['СКОЛЬКО ПОПУГАЕВ?', 'СКОЛЬКО ПОПУГАЕВ', 'СКОЛЬКО?', 'СКОЛЬКО'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').parrotCount,
   }, {
     messages: ['ЕСТЬ КТО ЖИВОЙ?', 'ЕСТЬ КТО ЖИВОЙ', 'ЕСТЬ КТО', 'ЕСТЬ КТО?', 'КТО ЖИВОЙ?', 'КТО ЖИВОЙ'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').userCount,
   }, {
     messages: ['LOG'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').log,
   }, {
     messages: ['PPM'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').ppm,
   }, {
     messages: ['COMMANDS'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').commands,
   }, {
     messages: ['CHANGELOG'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').changelog,
   },
@@ -56,20 +66,30 @@ const messages = [
   {
     messages: ['ИЛИТА', 'КТО ИЛИТА', 'КТО ИЛИТА?'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./statistic').elite,
   },
   {
     messages: ['HOT'],
     startFrom: false,
+    entrance: false,
     channels: ['test', 'friday', 'direct'],
     callback: require('./reddit').redditHot,
   },
   {
     messages: ['RDT'],
     startFrom: false,
-    channels: ['test', 'direct',],
+    entrance: false,
+    channels: ['test', 'direct'],
     callback: require('./reddit').redditJs,
+  },
+  {
+    messages: ['МИЛИЦИ', 'ПОЛИЦИ', 'ГОСПОДИН ПОЛИЦЕЙСКИЙ'],
+    startFrom: false,
+    entrance: true,
+    channels: ['test', 'friday', 'direct'],
+    callback: require('./police').police,
   },
 
 ];
---
+++ server/bot/commands/police.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const customResponse = [
+  'не хорошо так делать.',
+  'пэрротов на вас нет!',
+  'еще раз и больше ни разу.',
+  'не нарушаете пожалуйста, иначе на вас обрушится `гнев пэрротов`.',
+  'не нарушайте, а то ночью вам приснится кошмар от том, как вы стали ВУЙ-разработчиком.',
+  'а-та-та.',
+];
+
+
+function getPolice(text, callback, msg) {
+  const attachment = {};
+  attachment.username = 'милиция';
+  attachment.icon_emoji = ':warneng:';
+  const reg = new RegExp(msg);
+  const myRegexpUser = /@\w+/g;
+  const matchUser = myRegexpUser.exec(text);
+
+  if (reg.test(text)) {
+    if (matchUser) {
+      const randomResponse = Math.floor(Math.random() * (customResponse.length - 1));
+      console.log(randomResponse);
+      const message = `Господин полицейский к Вашим услугам.\nСпасибо за обращение.\n<${matchUser[0]}>, ${customResponse[randomResponse]}`;
+      callback(message, {}, attachment);
+    } else {
+      callback('Господин полицейский всегда на страже закона.\nЕсли у вас жалоба на конкретного человека, то повторите команду и укажите его @username', {}, attachment);
+    }
+  }
+}
+
+module.exports = {
+  police: (text, callback, msg) => {
+    getPolice(text, callback, msg);
+  },
+};
