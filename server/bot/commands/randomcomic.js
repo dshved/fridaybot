5213d93078eb104a7230121de0963ff55447fd9c
Dmitry Shved <dshved@hotmail.com>
2017-07-31 14:30:29 +0300

    Added random comic

---
+++ server/bot/commands/randomcomic.js
@@ -0,0 +1,39 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const iconv = require('iconv-lite');
+
+function getRandomComic(text, callback) {
+  request(
+    {
+      url: `http://explosm.net/rcg`,
+      encoding: null,
+    },
+    (err, res, body) => {
+      const $ = cheerio.load(iconv.decode(body, 'utf-8'), {
+        decodeEntities: false,
+      });
+      const image = $('#rcg_image');
+      const imageSrc = image.attr('value');
+      const attachment = {};
+      attachment.username = `Random Comic`;
+      attachment.icon_emoji = ':fbf:';
+      if (imageSrc) {
+        attachment.attachments = [
+          {
+            fallback: 'Random Comic',
+            color: '#36a64f',
+            title: 'Random Comic',
+            image_url: imageSrc,
+          },
+        ];
+        callback('', {}, attachment);
+      }
+    },
+  );
+}
+
+module.exports = {
+  randomComic: (text, callback, msg) => {
+    getRandomComic(text, callback);
+  },
+};
