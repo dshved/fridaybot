611cf63f15d0927bee4d51f0642739d583a69fd9
Dmitry Shved <dshved@hotmail.com>
2017-07-31 15:05:56 +0300

    Fixed random commic url

--- server/bot/commands/randomcomic.js
+++ server/bot/commands/randomcomic.js
@@ -3,27 +3,30 @@ const request = require('request');
 const iconv = require('iconv-lite');
 
 function getRandomComic(text, callback) {
+  const randomComic = Math.floor(Math.random() * (4650 - 1000 + 1) + 1000);
   request(
     {
-      url: `http://explosm.net/rcg`,
+      url: `http://explosm.net/comics/${randomComic}/`,
       encoding: null,
     },
     (err, res, body) => {
       const $ = cheerio.load(iconv.decode(body, 'utf-8'), {
         decodeEntities: false,
       });
-      const image = $('#rcg_image');
-      const imageSrc = image.attr('value');
+      const image = $('#main-comic');
+      let imageSrc = image.attr('src');
+
       const attachment = {};
-      attachment.username = `Fridaybot`;
+      attachment.username = `fridaybot`;
       attachment.icon_emoji = ':fbf:';
       if (imageSrc) {
+        imageSrc = imageSrc.substr(2);
         attachment.attachments = [
           {
             fallback: 'Random Comic',
             color: '#36a64f',
             title: 'Random Comic',
-            image_url: imageSrc,
+            image_url: `http://${imageSrc}`,
           },
         ];
         callback('', {}, attachment);
