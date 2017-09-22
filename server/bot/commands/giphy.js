7dc6b0b509733e2f2dddfaad7b885fa7c798cf60
Dmitry Shved <dshved@hotmail.com>
2017-09-22 14:42:07 +0300

    Added giphy command

---
+++ server/bot/commands/giphy.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+const getGiphy = (text, callback) => {
+  const url = `http://api.giphy.com/v1/gifs/random?api_key=a81da363e2174657a4d2a5c4091d303e&tag=${text}`;
+
+  request(
+    {
+      url: url,
+      encoding: null,
+    },
+    (err, res, body) => {
+      const json = JSON.parse(body);
+      const attachment = {};
+      attachment.username = `giphy`;
+      attachment.icon_emoji = ':giphy:';
+      if (json.meta.status == 200) {
+        attachment.attachments = [
+          {
+            fallback: 'Giphy',
+            color: '#36a64f',
+            title: 'Giphy',
+            image_url: json.data.image_url,
+          },
+        ];
+        callback('', {}, attachment);
+      }
+    },
+  );
+};
+
+module.exports = {
+  getGiphy: (text, callback) => {
+    getGiphy(text, callback);
+  },
+};
