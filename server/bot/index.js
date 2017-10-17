be8c02f77b053ca9fa7d8acc0559aaf74c538a12
Dmitry Shved <dshved@hotmail.com>
2017-10-17 11:19:39 +0300

    Remove status bot command

--- server/bot/index.js
+++ server/bot/index.js
@@ -13,7 +13,6 @@ const getSticker = require('./commands/sticker').getSticker;
 const sayText = require('./commands/say').sayText;
 const Log = require('./../models/log').Log;
 
-const activeBot = require('./commands/active.js').activeBot;
 const deleteParrots = require('./commands/delmessages').deleteParrots;
 const userJoin = require('./commands/userJoin').userJoin;
 const userLeave = require('./commands/userLeave').userLeave;
@@ -156,7 +155,7 @@ bot2.on('message', data => {
     }
   }
 });
-let start = true;
+
 bot.on('message', data => {
   // if (data.subtype === 'message_changed') {
   //   data.text = data.message.text;
@@ -177,15 +176,105 @@ bot.on('message', data => {
   global.io.emit('data', data);
   // console.log(data);
   let countParrots = 0;
+
   if (data.text) {
-    if (~data.text.toUpperCase().indexOf('СТАТУС БОТА') == -1) {
-      let message = start ? 'Работает' : 'Выключен';
-      bot.postMessageToChannel(botParams.channelName, message, messageParams);
+    if (data.channel === botParams.channelId) {
+      const matches = data.text.match(/:fp:|parrot/g);
+
+      if (matches !== null) {
+        botParams.parrotCount += matches.length;
+        countParrots += matches.length;
+      }
+
+      BotSettings.update(
+        { name: messageParams.username },
+        { parrot_counts: botParams.parrotCount },
+      ).then();
+      global.io.emit('parrot count', botParams.parrotCount);
+
+      const user = data.user ? data.user : data.bot_id;
+      const statistic = new Statistics({
+        event_type: 'user_message',
+        user_id: user,
+        timestamp: data.ts,
+        parrot_count: countParrots,
+      });
+
+      statistic.save();
     }
   }
-  if (data.text && !start) {
-    if (~data.text.toUpperCase().indexOf('ВКЛЮЧИТЬ БОТА') == -1) {
-      start = true;
+
+  if (data.text && data.subtype !== 'bot_message') {
+    if (~data.text.toUpperCase().indexOf('ПОВТОРИ ') == -1) {
+      const userText = data.text.substr(8);
+      const att = isThread(data, messageParams);
+      bot.postMessageToChannel(botParams.channelName, userText, att);
+      accessBotPost = true;
+    }
+  }
+
+  if (data.text) {
+    if (~data.text.toUpperCase().indexOf('MASK ') == -1) {
+      const userText = data.text.substr(5);
+      const userTextArray = userText.split(' ');
+      const userID = userTextArray[0].slice(2, -1);
+      if (userID) {
+        request(
+          {
+            url: `https://slack.com/api/users.info?token=${config.bot
+              .token}&user=${userID}&pretty=1`,
+            encoding: null,
+          },
+          (err, res, body) => {
+            const json = JSON.parse(body);
+            if (json.ok) {
+              const attachment = {};
+              attachment.username = json.user.name;
+              attachment.icon_url = json.user.profile.image_72;
+
+              let message = '';
+              for (var i = 1; i < userTextArray.length; i++) {
+                message += userTextArray[i] + ' ';
+              }
+              bot.postMessageToChannel(
+                botParams.channelName,
+                message,
+                attachment,
+              );
+            }
+          },
+        );
+      }
+    }
+  }
+
+  if (data.text) {
+    if (~data.text.toUpperCase().indexOf('МАСКА ') == -1) {
+      const userText = data.text.substr(6);
+      const say = require('./commands/sayHow').parseMessage;
+      let { message, attachment } = say(userText);
+      bot.postMessageToChannel(botParams.channelName, message, attachment);
+    }
+  }
+
+  if (data.text) {
+    if (~data.text.toUpperCase().indexOf('СОВЕРШИТЬ БОЛЬШУЮ ГЛУПОСТЬ') == -1) {
+      deleteParrots(botParams.channelId);
+    }
+  }
+
+  if (data.text) {
+    if (~data.text.toUpperCase().indexOf('CLEAR') == -1) {
+      deleteParrots(botParams.channelId);
+    }
+  }
+
+  if (data.text) {
+    if (~data.text.toUpperCase().indexOf('ФАЙЛ ') == -1) {
+      let url = data.text
+        .substr(6)
+        .replace(/</g, '')
+        .replace(/>/g, '');
       const message = '';
       const attachment = {};
       attachment.username = 'fridaybot';
@@ -193,292 +282,162 @@ bot.on('message', data => {
       attachment.attachments = [
         {
           fallback: '',
-          image_url: `https://avatanplus.com/files/resources/mid/578d74d9ced70156009072f7.png`,
+          image_url: `${url}`,
         },
       ];
       bot.postMessageToChannel(botParams.channelName, message, attachment);
     }
   }
 
-  if (start) {
-    if (data.text) {
-      if (data.channel === botParams.channelId) {
-        const matches = data.text.match(/:fp:|parrot/g);
+  if (data.text) {
+    data.text = data.text.toUpperCase();
 
-        if (matches !== null) {
-          botParams.parrotCount += matches.length;
-          countParrots += matches.length;
-        }
-
-        BotSettings.update(
-          { name: messageParams.username },
-          { parrot_counts: botParams.parrotCount },
-        ).then();
-        global.io.emit('parrot count', botParams.parrotCount);
-
-        const user = data.user ? data.user : data.bot_id;
-        const statistic = new Statistics({
-          event_type: 'user_message',
-          user_id: user,
-          timestamp: data.ts,
-          parrot_count: countParrots,
-        });
-
-        statistic.save();
-      }
-    }
-
-    if (data.text && data.subtype !== 'bot_message') {
-      if (~data.text.toUpperCase().indexOf('ПОВТОРИ ') == -1) {
-        const userText = data.text.substr(8);
-        const att = isThread(data, messageParams);
-        bot.postMessageToChannel(botParams.channelName, userText, att);
-        accessBotPost = true;
-      }
-    }
-
-    if (data.text) {
-      if (~data.text.toUpperCase().indexOf('MASK ') == -1) {
-        const userText = data.text.substr(5);
-        const userTextArray = userText.split(' ');
-        const userID = userTextArray[0].slice(2, -1);
-        if (userID) {
-          request(
-            {
-              url: `https://slack.com/api/users.info?token=${config.bot
-                .token}&user=${userID}&pretty=1`,
-              encoding: null,
-            },
-            (err, res, body) => {
-              const json = JSON.parse(body);
-              if (json.ok) {
-                const attachment = {};
-                attachment.username = json.user.name;
-                attachment.icon_url = json.user.profile.image_72;
-
-                let message = '';
-                for (var i = 1; i < userTextArray.length; i++) {
-                  message += userTextArray[i] + ' ';
-                }
-                bot.postMessageToChannel(
-                  botParams.channelName,
-                  message,
-                  attachment,
-                );
-              }
-            },
-          );
-        }
-      }
-    }
-
-    if (data.text) {
-      if (~data.text.toUpperCase().indexOf('МАСКА ') == -1) {
-        const userText = data.text.substr(6);
-        const say = require('./commands/sayHow').parseMessage;
-        let { message, attachment } = say(userText);
-        bot.postMessageToChannel(botParams.channelName, message, attachment);
-      }
-    }
-
-    if (data.text) {
-      if (~data.text.toUpperCase().indexOf('ВЫКЛЮЧИТЬ БОТА') == -1) {
-        start = false;
-        activeBot(botParams.channelId);
-      }
-    }
-
-    if (data.text) {
-      if (
-        ~data.text.toUpperCase().indexOf('СОВЕРШИТЬ БОЛЬШУЮ ГЛУПОСТЬ') == -1
-      ) {
-        deleteParrots(botParams.channelId);
-      }
-    }
-
-    if (data.text) {
-      if (~data.text.toUpperCase().indexOf('CLEAR') == -1) {
-        deleteParrots(botParams.channelId);
-      }
-    }
-
-    if (data.text) {
-      if (~data.text.toUpperCase().indexOf('ФАЙЛ ') == -1) {
-        let url = data.text
-          .substr(6)
-          .replace(/</g, '')
-          .replace(/>/g, '');
-        const message = '';
-        const attachment = {};
-        attachment.username = 'fridaybot';
-        attachment.icon_emoji = ':fridaybot_new:';
-        attachment.attachments = [
-          {
-            fallback: '',
-            image_url: `${url}`,
-          },
-        ];
-        bot.postMessageToChannel(botParams.channelName, message, attachment);
-      }
-    }
-
-    if (data.text) {
-      data.text = data.text.toUpperCase();
-
-      const channel = channelName(data);
-      const user = data.user ? data.user : data.bot_id;
-      UserMessages.findOne({ user_id: user }).then(result => {
-        if (result) {
-          botResponse.userMessageRes(
-            data,
-            channel,
-            (text, error, attachment) => {
-              if (!error.message) {
-                if (attachment) {
-                  sendToWhom(data, text, attachment);
-                } else {
-                  sendToWhom(data, text);
-                }
-              } else {
-                sendToWhom(data, `<@${data.user}> ${error.message}`);
-              }
-            },
-          );
-        }
-      });
-    }
-
-    if (data.text) {
-      data.text = data.text.toUpperCase();
-    }
-
-    if (data.text) {
-      if (~data.text.indexOf('UPTIME') == -1) {
-        var millisecToTimeStruct = function(millisec) {
-          var days, hours, minutes, seconds;
-          if (isNaN(millisec)) {
-            return {};
+    const channel = channelName(data);
+    const user = data.user ? data.user : data.bot_id;
+    UserMessages.findOne({ user_id: user }).then(result => {
+      if (result) {
+        botResponse.userMessageRes(data, channel, (text, error, attachment) => {
+          if (!error.message) {
+            if (attachment) {
+              sendToWhom(data, text, attachment);
+            } else {
+              sendToWhom(data, text);
+            }
+          } else {
+            sendToWhom(data, `<@${data.user}> ${error.message}`);
           }
-          days = millisec / (60 * 60 * 24);
-          hours = (days - ~~days) * 24;
-          minutes = (hours - ~~hours) * 60;
-          seconds = (minutes - ~~minutes) * 60;
-          return `${~~days}:${~~hours}:${~~minutes}:${~~seconds}`;
-        };
-        const uptime = process.uptime();
-        sendToWhom(data, millisecToTimeStruct(uptime));
+        });
       }
-    }
+    });
+  }
 
-    if (data.text === 'ПАРОЛЬ') {
-      console.log(data.user);
-    }
+  if (data.text) {
+    data.text = data.text.toUpperCase();
+  }
 
-    if (
-      data.subtype === 'channel_leave' &&
-      data.channel === botParams.channelId
-    ) {
-      userLeave(data, botParams, message => {
-        if (messageParams.thread_ts) {
-          delete messageParams['thread_ts'];
+  if (data.text) {
+    if (~data.text.indexOf('UPTIME') == -1) {
+      var millisecToTimeStruct = function(millisec) {
+        var days, hours, minutes, seconds;
+        if (isNaN(millisec)) {
+          return {};
         }
-        bot.postMessageToChannel(botParams.channelName, message, messageParams);
-      });
-      const statistic = new Statistics({
-        event_type: 'channel_leave',
-        user_id: data.user,
-        timestamp: data.ts,
-      });
-
-      statistic.save();
+        days = millisec / (60 * 60 * 24);
+        hours = (days - ~~days) * 24;
+        minutes = (hours - ~~hours) * 60;
+        seconds = (minutes - ~~minutes) * 60;
+        return `${~~days}:${~~hours}:${~~minutes}:${~~seconds}`;
+      };
+      const uptime = process.uptime();
+      sendToWhom(data, millisecToTimeStruct(uptime));
     }
+  }
 
-    if (
-      data.subtype === 'channel_join' &&
-      data.channel === botParams.channelId
-    ) {
-      userJoin(data, botParams, message => {
-        if (messageParams.thread_ts) {
-          delete messageParams['thread_ts'];
-        }
-        bot.postMessageToChannel(botParams.channelName, message, messageParams);
-      });
+  if (data.text === 'ПАРОЛЬ') {
+    console.log(data.user);
+  }
 
-      const statistic = new Statistics({
-        event_type: 'channel_join',
-        user_id: data.user,
-        timestamp: data.ts,
-      });
+  if (
+    data.subtype === 'channel_leave' &&
+    data.channel === botParams.channelId
+  ) {
+    userLeave(data, botParams, message => {
+      if (messageParams.thread_ts) {
+        delete messageParams['thread_ts'];
+      }
+      bot.postMessageToChannel(botParams.channelName, message, messageParams);
+    });
+    const statistic = new Statistics({
+      event_type: 'channel_leave',
+      user_id: data.user,
+      timestamp: data.ts,
+    });
 
-      statistic.save();
-    }
-    if (
-      data.type === 'message' &&
-      data.channel === botParams.channelId &&
-      accessBotPost &&
-      data.subtype !== 'channel_leave'
-    ) {
-      BotMessages.findOne({ user_message: data.text }).then(result => {
-        if (result) {
-          const att = isThread(data, messageParams);
-          bot.postMessageToChannel(
-            botParams.channelName,
-            result.bot_message,
-            att,
-          );
-          accessBotPost = false;
-          saveLog(data);
-        }
-      });
-    }
+    statistic.save();
+  }
 
-    if (
-      data.type === 'message' &&
-      data.channel === botParams.channelId &&
-      data.subtype !== 'bot_message' &&
-      data.subtype !== 'channel_leave'
-    ) {
-      BotMessages.findOne({ user_message: data.text }).then(result => {
-        if (result) {
-          const att = isThread(data, messageParams);
-          bot.postMessageToChannel(
-            botParams.channelName,
-            result.bot_message,
-            att,
-          );
-          saveLog(data);
-        }
-      });
-      getSticker(data, att => {
-        const attr = isThread(data, att);
-        bot.postMessageToChannel(botParams.channelName, '', attr);
-      });
-      UserMessages.findOne({ user_id: data.user }).then(result => {
-        if (!result) {
-          bot.getUserById(data.user).then(d => {
-            const newMessage = new UserMessages({
-              user_id: d.id,
-              user_name: d.name,
-              user_full_name: d.real_name,
-              count_messages: 1,
-              count_parrots: 0,
-            });
-            newMessage.save(d.id);
+  if (data.subtype === 'channel_join' && data.channel === botParams.channelId) {
+    userJoin(data, botParams, message => {
+      if (messageParams.thread_ts) {
+        delete messageParams['thread_ts'];
+      }
+      bot.postMessageToChannel(botParams.channelName, message, messageParams);
+    });
+
+    const statistic = new Statistics({
+      event_type: 'channel_join',
+      user_id: data.user,
+      timestamp: data.ts,
+    });
+
+    statistic.save();
+  }
+  if (
+    data.type === 'message' &&
+    data.channel === botParams.channelId &&
+    accessBotPost &&
+    data.subtype !== 'channel_leave'
+  ) {
+    BotMessages.findOne({ user_message: data.text }).then(result => {
+      if (result) {
+        const att = isThread(data, messageParams);
+        bot.postMessageToChannel(
+          botParams.channelName,
+          result.bot_message,
+          att,
+        );
+        accessBotPost = false;
+        saveLog(data);
+      }
+    });
+  }
+
+  if (
+    data.type === 'message' &&
+    data.channel === botParams.channelId &&
+    data.subtype !== 'bot_message' &&
+    data.subtype !== 'channel_leave'
+  ) {
+    BotMessages.findOne({ user_message: data.text }).then(result => {
+      if (result) {
+        const att = isThread(data, messageParams);
+        bot.postMessageToChannel(
+          botParams.channelName,
+          result.bot_message,
+          att,
+        );
+        saveLog(data);
+      }
+    });
+    getSticker(data, att => {
+      const attr = isThread(data, att);
+      bot.postMessageToChannel(botParams.channelName, '', attr);
+    });
+    UserMessages.findOne({ user_id: data.user }).then(result => {
+      if (!result) {
+        bot.getUserById(data.user).then(d => {
+          const newMessage = new UserMessages({
+            user_id: d.id,
+            user_name: d.name,
+            user_full_name: d.real_name,
+            count_messages: 1,
+            count_parrots: 0,
           });
-        } else {
-          const newCountParrots = result.count_parrots + countParrots;
-          const newCountMessages = result.count_messages + 1;
+          newMessage.save(d.id);
+        });
+      } else {
+        const newCountParrots = result.count_parrots + countParrots;
+        const newCountMessages = result.count_messages + 1;
 
-          UserMessages.findOneAndUpdate(
-            { user_id: data.user },
-            {
-              count_parrots: newCountParrots,
-              count_messages: newCountMessages,
-            },
-          ).then();
-        }
-      });
-    }
+        UserMessages.findOneAndUpdate(
+          { user_id: data.user },
+          {
+            count_parrots: newCountParrots,
+            count_messages: newCountMessages,
+          },
+        ).then();
+      }
+    });
   }
 });
 
