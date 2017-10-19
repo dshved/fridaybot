1ab13a433346e49eef7224d3aba3cf59a6e439b6
Dmitry Shved <dshved@hotmail.com>
2017-10-19 14:16:01 +0300

    Added police route

--- server/routers/router.js
+++ server/routers/router.js
@@ -4,6 +4,9 @@ const Auth = require('../middlewares/auth');
 const BotMessages = require('../models/botmessage').BotMessages;
 const BotSettings = require('../models/botsetting').BotSettings;
 const Stickers = require('../models/sticker').Sticker;
+const Jimp = require('jimp');
+const config = require('./../../config.js');
+const request = require('request');
 
 router.get('/', (req, res, next) => {
   if (!req.session.token) {
@@ -37,6 +40,49 @@ router.get('/home', Auth, (req, res, next) => {
       });
     }
   });
+});
+
+router.get('/police/:id', (req, res, next) => {
+  function promiseRequest(url) {
+    return new Promise((resolve, reject) => {
+      request(url, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(body);
+      });
+    });
+  }
+
+  async function getImage(src) {
+    return await Jimp.read(src);
+  }
+
+  async function draw() {
+    const response = await promiseRequest({
+      url: `https://slack.com/api/users.info?token=${config.bot
+        .token}&user=${req.params.id}&pretty=1`,
+      encoding: null,
+    });
+    const { user, ok } = JSON.parse(response);
+    if (!ok) {
+      res.send('error');
+      return;
+    }
+    const baseImg = await Jimp.read('./public/images/police/mask_1.png');
+    let temp = await getImage(user.profile.image_48);
+    baseImg.composite(temp, 100, 90);
+    const endImg = await Jimp.read('./public/images/police/mask_1.png');
+    baseImg.composite(endImg, 0, 0);
+    baseImg.getBase64(Jimp.MIME_PNG, function(err, base) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<html><body><img src="');
+      res.write(base);
+      res.end('"/></body></html>');
+    });
+  }
+
+  draw();
 });
 
 module.exports = router;
