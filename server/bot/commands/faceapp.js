d001895d60e20ec5a87ad893e42698bfe622a3d2
Dmitry Shved <dshved@hotmail.com>
2017-11-01 10:51:45 +0300

    Added faceapp

---
+++ server/bot/commands/faceapp.js
@@ -0,0 +1,100 @@
+const request = require('request');
+const fs = require('fs');
+const faceapp = require('faceapp');
+const { promisify } = require('util');
+const writeFileAsync = promisify(fs.writeFile);
+
+const config = require('../../../config');
+
+function promiseRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(body);
+    });
+  });
+}
+
+const draw = async (mask, text, callback) => {
+  const myRegexpUser = /@\w+/;
+  const matchUser = text.match(myRegexpUser);
+  if (matchUser) {
+    const userId = matchUser[0].substring(1);
+    const randomName = `${userId}-${mask}-${Math.random()
+      .toString(36)
+      .substring(2)}.jpg`;
+    const response = await promiseRequest({
+      url: `https://slack.com/api/users.info?token=${config.bot
+        .token}&user=${userId}&pretty=1`,
+      encoding: null,
+    });
+    const { user, ok } = JSON.parse(response);
+    if (!ok) {
+      return;
+    }
+    const userImageUrl = user.profile.image_192
+      .replace(/192\./, '1024.')
+      .replace(/=192/, '=1024.');
+    let bufferImage;
+    let image;
+    try {
+      bufferImage = await promiseRequest({ url: userImageUrl, encoding: null });
+    } catch (err) {
+      return;
+    }
+    try {
+      image = await faceapp.process(bufferImage, mask);
+
+      await writeFileAsync(`./public/uploads/mask/${randomName}`, image);
+      const attachment = {
+        username: 'fridaybot',
+        icon_emoji: ':fridaybot_new:',
+        attachments: [
+          {
+            fallback: 'Faceapp',
+            color: '#ff0000',
+            image_url: `https://fridaybot.tk/uploads/mask/${randomName}`,
+          },
+        ],
+      };
+      callback(mask, {}, attachment);
+    } catch (err) {
+      callback(err.message, {});
+    }
+  }
+};
+
+module.exports = {
+  drawSmile: (text, callback) => {
+    draw('smile', text, callback);
+  },
+  drawSmile2: (text, callback) => {
+    draw('smile_2', text, callback);
+  },
+  drawHot: (text, callback) => {
+    draw('hot', text, callback);
+  },
+  drawOld: (text, callback) => {
+    draw('old', text, callback);
+  },
+  drawYoung: (text, callback) => {
+    draw('young', text, callback);
+  },
+  drawFemale2: (text, callback) => {
+    draw('female_2', text, callback);
+  },
+  drawFemale: (text, callback) => {
+    draw('female', text, callback);
+  },
+  drawMale: (text, callback) => {
+    draw('male', text, callback);
+  },
+  drawPan: (text, callback) => {
+    draw('pan', text, callback);
+  },
+  drawHitman: (text, callback) => {
+    draw('hitman', text, callback);
+  },
+};
