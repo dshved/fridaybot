edf4f5275bc31c4002da59f04c2195520e63849b
Dmitry Shved <dshved@hotmail.com>
2017-12-05 14:28:05 +0300

    Fixed count users

--- server/bot/commands/userLeave.js
+++ server/bot/commands/userLeave.js
@@ -1,9 +1,9 @@
-const request = require('request');
 const BotSettings = require('./../../models/botsetting').BotSettings;
 const UserMessages = require('./../../models/usermessage').UserMessages;
 const sayText = require('./../commands/say').sayText;
+const getUserCount = require('./userCount');
 
-async function userLeave(data, botParams, cb) {
+async function userLeave(data, { channelId }, cb) {
   const settings = await BotSettings.findOne();
   const userInfo = await UserMessages.findOne({ user_id: data.user });
 
@@ -12,29 +12,20 @@ async function userLeave(data, botParams, cb) {
       .replace(/first_name/g, data.user_profile.first_name)
       .replace(/real_name/g, data.user_profile.real_name)
       .replace(/name/g, `<@${data.user_profile.name}>`);
-    request(
-      {
-        url: `https://slack.com/api/channels.info?token=${global.BOT_TOKEN}&channel=${botParams.channelId}`,
-        encoding: null,
-      },
-      (err, res, body) => {
-        const json = JSON.parse(body);
-        if (json.ok) {
-          const countUsers = json.channel.members.length;
-          sayText(`:RAGE: ${countUsers}`, true, 10, false, callback => {
-            const message = callback + leaveMessage;
-            let userStatistics = '';
-            const countMessages = userInfo.count_messages;
-            const countParrots = userInfo.count_parrots;
-            userStatistics = `\nПользователем было отправлено:\nсообщений - ${countMessages}\nпэрротов - ${countParrots}`;
-            UserMessages.remove({ user_id: data.user }).then();
-            cb(message + userStatistics);
-          });
-        } else {
-          cb(leaveMessage);
-        }
-      },
-    );
+    const countUsers = await getUserCount(channelId);
+    if (countUsers) {
+      sayText(`:RAGE: ${countUsers}`, true, 10, false, callback => {
+        const message = callback + leaveMessage;
+        let userStatistics = '';
+        const countMessages = userInfo.count_messages;
+        const countParrots = userInfo.count_parrots;
+        userStatistics = `\nПользователем было отправлено:\nсообщений - ${countMessages}\nпэрротов - ${countParrots}`;
+        UserMessages.remove({ user_id: data.user }).then();
+        cb(message + userStatistics);
+      });
+    } else {
+      cb(leaveMessage);
+    }
   }
 }
 
