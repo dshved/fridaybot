f7a96b7888bdfaa1730110cabe177a9ffcf43313
Dmitry Shved <dshved@hotmail.com>
2017-12-18 17:41:41 +0300

    Updated

--- server/bot/commands/fewParrotsText.js
+++ server/bot/commands/fewParrotsText.js
@@ -1,4 +1,4 @@
 module.exports = {
   message:
-    ':cptl::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cptr:\n:cpl::hh::u_rus::gg_rus::hh::oo::sp::b_rus::oo::l_rus::mz_rus::sh_rus::ee::sp::p_rus::oo::p_rus::cpr:\n:cpl::u_rus::g_rus::aa::ee::bb::sp::b_rus::l_rus::ee::aa::tt::mz_rus::ex_point::ex_point::ex_point::ex_point::cpr:\n:cpbl::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpbr:',
+    ':cptl::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cpt::cptr:\n:cpl::hh::u_rus::gg_rus::hh::oo::sp::b_rus::oo::l_rus::mz_rus::sh_rus::ee::sp::sp::sp::sp::cpr:\n:cpl::p_rus::oo::p_rus::u_rus::g_rus::aa::ee::bb::sp::b_rus::l_rus::ee::aa::tt::mz_rus::ex_point::cpr:\n:cpbl::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpb::cpbr:',
 };
