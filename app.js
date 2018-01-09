86ec17cd48783a8924296726033cf5a75e47ff0b
Dmitry Shved <dshved@hotmail.com>
2018-01-09 13:06:34 +0300

    Updated 404 page

--- app.js
+++ app.js
@@ -84,18 +84,8 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
+app.use((req, res) => {
+  res.status(404).render('error');
 });
 
 module.exports = { app, server };
