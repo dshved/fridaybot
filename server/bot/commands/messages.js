9ff9efb91be61a319aa48f6b6be0b54a5f59d559
Dmitriy Shved <dshved@hotmail.com>
2018-02-06 12:33:48 +0300

    Updated party command

--- server/bot/commands/messages.js
+++ server/bot/commands/messages.js
@@ -466,13 +466,7 @@ const messages = [
     callback: require('./statistic').whenDrinkUp,
   },
   {
-    messages: [
-      'PARTY ',
-      'ПАТИ ',
-      'ПРОКАТИ ',
-      'СЕМИРУЛЬНИК ПРОКАТИ ',
-      'СЕМИРУЛЬНИК ПРОКАТИ',
-    ],
+    messages: ['PARTY ', 'ПАТИ ', 'ПРОКАТИ ', 'СЕМИРУЛЬНИК ПРОКАТИ '],
     startFrom: true,
     entrance: false,
     channels: ['test', 'friday', 'direct'],
